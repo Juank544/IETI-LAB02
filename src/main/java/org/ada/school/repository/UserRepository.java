9cd50a5220856ff6126ff8e6482ed300ad8eaa84
Juank544 <camilo.gv@live.com>
2022-02-10 00:35:29 -0500

    Parte 3

---
+++ src/main/java/org/ada/school/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.ada.school.repository;
+
+import org.ada.school.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findById(String id);
+}
